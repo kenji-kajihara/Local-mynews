08436553edc066dbbd7a59e208f488b612b7dec2
kenji-kajihara <kenji.kajihara712@gmail.com>
2019-07-10 11:22:07 +0900

    PHPLarael 10 task

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -10,21 +10,21 @@ class ProfileController extends Controller
     //
     public function add()
     {
-      return view('admin.profile.create')
+      return view('admin.profile.create');
     }
 
     public function create()
     {
-      return redirect('admin/profile/create')
+      return redirect('admin/profile/create');
     }
 
     public function edit()
     {
-      return view('admin.profile.create')
+      return view('admin.profile.create');
     }
 
     public function update()
     {
-      return redirect('admin/profile/create')
+      return redirect('admin/profile/create');
     }
 }
