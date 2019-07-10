a9c8bb1e86b972722391f042e4d3086e01f570f9
kenji-kajihara <kenji.kajihara712@gmail.com>
2019-07-10 13:28:14 +0900

    PHPLaravel 11 task

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-  Route:get('news/create','Admin\NewsController@add');
+  Route::get('news/create','Admin\NewsController@add');
 });
 
-Route::group(['prefix' =>'admin']),function() {
+Route::group(['prefix' =>'admin'], function() {
   Route::get('profile/create','Admin\ProfileController@add');
   Route::get('profile/edit','Admin\ProfileController@edit');
-}
+});
 
 Route::get('XXX','AAAController@bbb');
